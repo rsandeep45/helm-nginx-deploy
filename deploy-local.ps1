ce2ff1bd364604dcd92a0a02fab37d61f24d5bce
srepaka <sandeep.siarem@gmail.com>
2025-04-13 19:27:38 +0530

    commit v2

---
+++ deploy-local.ps1
@@ -0,0 +1,13 @@
+﻿# deploy-local.ps1
+
+# Start Minikube if not running
+if (-not (minikube status | Select-String "Running")) {
+    Write-Host "Starting Minikube..."
+    minikube start
+}
+
+# Set Docker env (optional for image builds)
+& minikube docker-env | Invoke-Expression
+
+# Deploy using Helm
+helm upgrade --install nginx ./charts/nginx --namespace default --create-namespace
---
+++ deploy-local.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+# deploy-local.sh
+
+minikube status || minikube start
+eval $(minikube docker-env)
+helm upgrade --install nginx ./charts/nginx \
+  --namespace default --create-namespace
